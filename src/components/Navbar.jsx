6cf4d1047384bab6f44439f0081ac6bfc5b1b6e5
Eyad <eyadessam96@gmail.com>
2024-05-08 12:16:23 -0400

    taking off resume

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -115,7 +115,7 @@ const Navbar = () => {
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]">
             <a
               className="flex justify-between items-center w-full text-gray-300"
-              href="https://drive.google.com/file/d/1SBZ8i6xu7_j7JeiOvnOiOHRgFXMZF3V8/view?usp=drive_link"
+              href="/"
             >
               Resume <BsFillPersonLinesFill size={30} />
             </a>
